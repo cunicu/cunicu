d75af4d52d1b7ede24216311b7998ebf953148e9
Steffen Vogel <post@steffenvogel.de>
2022-01-07 11:31:09 +0100

    add new usage template
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,39 @@ import (
 	"riasc.eu/wice/internal"
 )
 
+const (
+	usageTemplate = `Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+
+Credits:
+  Steffen Vogel <post@steffenvogel.de>
+
+Website:
+  https://github.com/stv0g/wice
+`
+)
+
 var (
 	// Used for flags.
 	CfgFile string
@@ -28,6 +61,7 @@ func init() {
 	)
 
 	rootCmd.Version = version
+	rootCmd.SetUsageTemplate(usageTemplate)
 }
 
 func setupLogging() {
