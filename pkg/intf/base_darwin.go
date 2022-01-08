6c7c66c62ba0b0f17e003b2367e09eeb03ca70ec
Steffen Vogel <post@steffenvogel.de>
2022-01-08 02:06:02 +0100

    add stub for darwin
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ pkg/intf/base_darwin.go
@@ -0,0 +1,15 @@
+package intf
+
+import (
+	"net"
+)
+
+func (i *BaseInterface) addAddress(ip *net.IPNet) error {
+	// TODO
+	return nil
+}
+
+func (i *BaseInterface) addRoute(dst *net.IPNet) error {
+	// TODO
+	return nil
+}
