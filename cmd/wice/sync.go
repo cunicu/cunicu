b17fe2fef31d66d224df7e780a2995bbe94e3f7f
Steffen Vogel <post@steffenvogel.de>
2022-01-12 14:59:15 +0100

    add "wice sync" command
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ cmd/wice/sync.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"riasc.eu/wice/pkg/pb"
+)
+
+var (
+	syncCmd = &cobra.Command{
+		Use:   "sync",
+		Short: "Synchronize interfaces",
+		Long:  "Synchronizes the internal daemon state with the state of the Wireguard interfaces",
+		RunE:  sync,
+		Args:  cobra.RangeArgs(0, 1),
+	}
+)
+
+func init() {
+	addClientCommand(rootCmd, syncCmd)
+}
+
+func sync(cmd *cobra.Command, args []string) error {
+	rerr, err := client.Sync(context.Background(), &pb.SyncParams{})
+	if err != nil {
+		return fmt.Errorf("failed RPC request: %w", err)
+	} else if !rerr.Ok() {
+		return fmt.Errorf("received RPC error: %w", rerr)
+	}
+
+	return nil
+}
