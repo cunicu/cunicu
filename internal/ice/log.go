e9e7f8122f54c85996ef3d6d67db6bba46003627
Steffen Vogel <post@steffenvogel.de>
2022-01-14 13:40:51 +0100

    ice: make log level configurable via env var
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- internal/ice/log.go
+++ internal/ice/log.go
@@ -1,11 +1,13 @@
 package ice
 
 import (
+	"os"
 	"unicode"
 
 	"github.com/pion/logging"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"riasc.eu/wice/internal/log"
 )
 
 type LoggerFactory struct {
@@ -71,9 +73,15 @@ func (f *LoggerFactory) hook(e zapcore.Entry) error {
 }
 
 func (f *LoggerFactory) NewLogger(scope string) logging.LeveledLogger {
+	levelStr := os.Getenv("PION_LOG")
+
+	var level zapcore.Level
+	level.UnmarshalText([]byte(levelStr))
+
 	logger := f.Base.Named("ice").WithOptions(
 		zap.Hooks(f.hook),
 		zap.Fields(zap.String("scope", scope)),
+		log.WithLevel(level),
 	)
 
 	return &LeveledLogger{
