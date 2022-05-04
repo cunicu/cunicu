3d11166ca596ba276bd8b1290ac41766702878b5
Steffen Vogel <post@steffenvogel.de>
2022-05-04 13:56:33 +0200

    add new errors sub-package
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/intf/kernel_others.go
+++ pkg/intf/kernel_others.go
@@ -2,6 +2,18 @@
 
 package intf
 
+import (
+	"golang.zx2c4.com/wireguard/wgctrl"
+	"riasc.eu/wice/internal/config"
+	"riasc.eu/wice/internal/errors"
+	"riasc.eu/wice/pkg/pb"
+	"riasc.eu/wice/pkg/signaling"
+)
+
 func WireguardModuleExists() bool {
 	return false
+}
+
+func CreateKernelInterface(name string, client *wgctrl.Client, backend signaling.Backend, events chan *pb.Event, cfg *config.Config) (Interface, error) {
+	return nil, errors.ErrNotSupported
 }
