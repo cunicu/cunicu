9e6154c8187695448e6aafe6ff254c5c6a692a73
Steffen Vogel <post@steffenvogel.de>
2022-05-17 12:48:37 +0200

    add new status sub-command
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ cmd/wice/status.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/encoding/protojson"
+	"riasc.eu/wice/pkg/pb"
+)
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Show current status of ɯice daemon",
+	RunE:  status,
+	Args:  cobra.NoArgs,
+}
+
+func init() {
+	addClientCommand(rootCmd, statusCmd)
+}
+
+func status(cmd *cobra.Command, args []string) error {
+	sts, err := client.GetStatus(context.Background(), &pb.Void{})
+	if err != nil {
+		logger.Fatal("Failed to retrieve status from daemon", zap.Error(err))
+	}
+
+	mo := protojson.MarshalOptions{
+		Multiline:       true,
+		Indent:          "  ",
+		AllowPartial:    true,
+		UseProtoNames:   true,
+		EmitUnpopulated: true,
+	}
+
+	buf, err := mo.Marshal(sts)
+	if err != nil {
+		logger.Fatal("Failed to marshal", zap.Error(err))
+	}
+
+	os.Stdout.Write(buf)
+
+	return nil
+}
