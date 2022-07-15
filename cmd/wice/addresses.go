24d2a7c8c4b82fe43903819611769bff86cb9173
Steffen Vogel <post@steffenvogel.de>
2022-07-15 23:51:00 +0200

    add new sub-command to calculate link-local addresses from public key
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ cmd/wice/addresses.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"riasc.eu/wice/pkg/crypto"
+)
+
+var (
+	addressesCmd = &cobra.Command{
+		Use:    "addresses",
+		Short:  "Calculate the IPv6 link-local address from a public key",
+		Run:    addresses,
+		Hidden: true,
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(addressesCmd)
+}
+
+func addresses(cmd *cobra.Command, args []string) {
+	logger := zap.L()
+
+	reader := bufio.NewReader(os.Stdin)
+	keyB64, _ := reader.ReadString('\n')
+
+	key, err := crypto.ParseKey(keyB64)
+	if err != nil {
+		logger.Fatal("Failed to parse key", zap.Error(err), zap.String("key", keyB64))
+	}
+
+	fmt.Println(key.IPv6Address().String())
+	fmt.Println(key.IPv4Address().String())
+}
