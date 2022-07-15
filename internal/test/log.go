8126012ad6547b182b5851bbd97c56cbaf6829d6
Steffen Vogel <post@steffenvogel.de>
2022-07-16 00:11:45 +0200

    tests: log test output also to a log file
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- internal/test/log.go
+++ internal/test/log.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"net/url"
+	"os"
 
 	"github.com/onsi/ginkgo/v2"
 	"go.uber.org/zap"
@@ -21,6 +22,10 @@ func (w *writerWrapper) Sync() error {
 }
 
 func SetupLogging() *zap.Logger {
+	return SetupLoggingWithFile("", false)
+}
+
+func SetupLoggingWithFile(fn string, truncate bool) *zap.Logger {
 	if err := zap.RegisterSink("ginkgo", func(u *url.URL) (zap.Sink, error) {
 		return &writerWrapper{ginkgo.GinkgoWriter}, nil
 	}); err != nil {
@@ -28,5 +33,14 @@ func SetupLogging() *zap.Logger {
 	}
 
 	outputPaths := []string{"ginkgo:"}
+
+	if truncate {
+		os.Truncate(fn, 0)
+	}
+
+	if fn != "" {
+		outputPaths = append(outputPaths, fn)
+	}
+
 	return log.SetupLogging(zap.DebugLevel, outputPaths, outputPaths)
 }
