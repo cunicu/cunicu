df70b8ab52d14c19e2fb5d99ca17571e80dd4d8d
Steffen Vogel <post@steffenvogel.de>
2022-07-16 22:48:06 +0200

    add route sync feature
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ pkg/feat/sync/routes/sync.go
@@ -0,0 +1,97 @@
+package routes
+
+import (
+	"errors"
+	"net"
+
+	"go.uber.org/zap"
+	"golang.org/x/sys/unix"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"riasc.eu/wice/pkg/core"
+	"riasc.eu/wice/pkg/watcher"
+)
+
+type gwHashV4 [2]byte
+type gwHashV6 [8]byte
+
+type Syncer struct {
+	watcher *watcher.Watcher
+
+	gwMapV4 map[gwHashV4]*core.Peer
+	gwMapV6 map[gwHashV6]*core.Peer
+
+	logger *zap.Logger
+}
+
+func New(w *watcher.Watcher, table string) (*Syncer, error) {
+	s := &Syncer{
+		watcher: w,
+		gwMapV4: map[gwHashV4]*core.Peer{},
+		gwMapV6: map[gwHashV6]*core.Peer{},
+		logger:  zap.L().Named("sync.routes"),
+	}
+
+	w.OnInterface.Register(s)
+
+	go s.watchKernel()
+
+	return s, nil
+}
+
+func (s *Syncer) OnInterfaceAdded(i *core.Interface) {
+	i.OnPeer.Register(s)
+}
+
+func (s *Syncer) OnPeerAdded(p *core.Peer) {
+	ipV4 := p.PublicKey().IPv4Address()
+	hashV4 := *(*gwHashV4)(ipV4.IP[14:])
+
+	ipV6 := p.PublicKey().IPv6Address()
+	hashV6 := *(*gwHashV6)(ipV6.IP[8:])
+
+	s.gwMapV4[hashV4] = p
+	s.gwMapV6[hashV6] = p
+
+	s.syncKernel() // Initial sync
+
+	p.OnModified.Register(s)
+}
+
+func (s *Syncer) OnPeerRemoved(p *core.Peer) {
+	ipV4 := p.PublicKey().IPv4Address()
+	hashV4 := *(*gwHashV4)(ipV4.IP[14:])
+
+	ipV6 := p.PublicKey().IPv6Address()
+	hashV6 := *(*gwHashV6)(ipV6.IP[8:])
+
+	delete(s.gwMapV4, hashV4)
+	delete(s.gwMapV6, hashV6)
+}
+
+func (s *Syncer) OnPeerModified(p *core.Peer, old *wgtypes.Peer, m core.PeerModifier, ipsAdded, ipsRemoved []net.IPNet) {
+	for _, dst := range ipsAdded {
+		if err := p.Interface.KernelDevice.AddRoute(&dst); err != nil {
+			s.logger.Error("Failed to add route", zap.Error(err))
+			continue
+		}
+
+		s.logger.Info("Added new AllowedIP to kernel routing table",
+			zap.String("dst", dst.String()),
+			zap.String("intf", p.Interface.Name()),
+			zap.Any("peer", p.PublicKey()))
+	}
+
+	for _, dst := range ipsRemoved {
+		if err := p.Interface.KernelDevice.DeleteRoute(&dst); err != nil && !errors.Is(err, unix.ESRCH) {
+			s.logger.Error("Failed to delete route", zap.Error(err))
+			continue
+		}
+
+		s.logger.Info("Remove vanished AllowedIP from kernel routing table",
+			zap.String("dst", dst.String()),
+			zap.String("intf", p.Interface.Name()),
+			zap.Any("peer", p.PublicKey()))
+	}
+}
+
+func (s *Syncer) OnInterfaceRemoved(i *core.Interface) {}
---
+++ pkg/feat/sync/routes/sync_linux.go
@@ -0,0 +1,95 @@
+package routes
+
+import (
+	"github.com/vishvananda/netlink"
+	"go.uber.org/zap"
+	"golang.org/x/sys/unix"
+	"riasc.eu/wice/pkg/device"
+)
+
+func (s *Syncer) syncKernel() {
+	routes, err := netlink.RouteList(nil, unix.AF_INET6)
+	if err != nil {
+		s.logger.Error("Failed to get routes from kernel", zap.Error(err))
+	}
+
+	for _, route := range routes {
+		s.handleRouteUpdate(&netlink.RouteUpdate{
+			Type:  unix.RTM_NEWROUTE,
+			Route: route,
+		})
+	}
+}
+
+func (s *Syncer) watchKernel() {
+	rus := make(chan netlink.RouteUpdate)
+	errs := make(chan error)
+
+	if err := netlink.RouteSubscribeWithOptions(rus, nil, netlink.RouteSubscribeOptions{
+		ErrorCallback: func(err error) {
+			errs <- err
+		},
+	}); err != nil {
+		s.logger.Error("Failed to subscribe to netlink route updates", zap.Error(err))
+		return
+	}
+
+	for {
+		select {
+		case ru := <-rus:
+			s.handleRouteUpdate(&ru)
+
+		case err := <-errs:
+			s.logger.Error("Failed to monitor kernel route updates", zap.Error(err))
+		}
+	}
+}
+
+func (s *Syncer) handleRouteUpdate(ru *netlink.RouteUpdate) {
+	s.logger.Debug("Received netlink route update", zap.Any("update", ru))
+
+	if ru.Protocol == device.RouteProtocol {
+		s.logger.Debug("Ignoring gateway which was installed by ourself", zap.Any("gw", ru.Gw))
+		return
+	}
+
+	if ru.Gw == nil {
+		s.logger.Debug("Ignoring route with missing gateway")
+		return
+	}
+
+	if ru.Gw.To16() == nil {
+		s.logger.Debug("Ignoring non-IPv6 gateway", zap.Any("gw", ru.Gw))
+		return
+	}
+
+	if !ru.Gw.IsLinkLocalUnicast() {
+		s.logger.Debug("Ignoring non-link-local gateway", zap.Any("gw", ru.Gw))
+		return
+	}
+
+	hash := *(*gwHashV6)(ru.Gw[8:])
+
+	p, ok := s.gwMapV6[hash]
+	if !ok {
+		s.logger.Debug("Ignoring unknown gateway", zap.Any("gw", ru.Gw))
+		return
+	}
+
+	if p.Interface.KernelDevice.Index() != ru.LinkIndex {
+		s.logger.Debug("Ignoring gateway due to interface mismatch", zap.Any("gw", ru.Gw))
+		return
+	}
+
+	switch ru.Type {
+	case unix.RTM_NEWROUTE:
+		if err := p.AddAllowedIP(ru.Dst); err != nil {
+			s.logger.Error("Failed to add allowed IP", zap.Error(err))
+		}
+
+	case unix.RTM_DELROUTE:
+		if err := p.RemoveAllowedIP(ru.Dst); err != nil {
+			s.logger.Error("Failed to remove allowed IP", zap.Error(err))
+		}
+	}
+}
