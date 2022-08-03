00b47efc8aae1e3dfa9737e025a5fedfc6174615
Steffen Vogel <post@steffenvogel.de>
2022-08-03 08:03:32 +0200

    device: add stub for windows
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ pkg/device/device_windows.go
@@ -0,0 +1,40 @@
+package device
+
+import (
+	"net"
+
+	"riasc.eu/wice/pkg/errors"
+)
+
+type WindowsKernelDevice struct {
+}
+
+func (d *WindowsKernelDevice) AddAddress(ip net.IPNet) error {
+	return errors.ErrNotSupported
+}
+
+func (d *WindowsKernelDevice) AddRoute(dst net.IPNet) error {
+	return errors.ErrNotSupported
+}
+
+func (d *WindowsKernelDevice) Index() int {
+	return -1
+}
+
+func (d *WindowsKernelDevice) MTU() int {
+	// MTU is a route attribute which we need to adjust for all routes added for the interface
+	return -1
+}
+
+func (d *WindowsKernelDevice) SetMTU(mtu int) error {
+	// MTU is a route attribute which we need to adjust for all routes added for the interface
+	return errors.ErrNotSupported
+}
+
+func (d *WindowsKernelDevice) SetUp() error {
+	return errors.ErrNotSupported
+}
+
+func (d *WindowsKernelDevice) SetDown() error {
+	return errors.ErrNotSupported
+}
