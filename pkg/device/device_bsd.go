4134cab2871ee508fa93e727523329013c8868f0
Steffen Vogel <post@steffenvogel.de>
2022-08-03 08:39:04 +0200

    device: finish FreeBSD kernel device support
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/device/device_bsd.go
+++ pkg/device/device_bsd.go
@@ -1,4 +1,4 @@
-// go:build darwin || freebsd
+//go:build darwin || freebsd
 
 package device
 
@@ -6,29 +6,66 @@ import (
 	"net"
 	"os/exec"
 
+	"go.uber.org/zap"
 	"riasc.eu/wice/pkg/errors"
 )
 
 type BSDKernelDevice struct {
-	index int
+	created bool
+	index   int
+	logger  *zap.Logger
 }
 
-func (d *BSDKernelDevice) AddAddress(ip net.IPNet) error {
-	i, err := net.InterfaceByIndex(d.index)
-	if err != nil {
-		return err
+func NewKernelDevice(name string) (KernelDevice, error) {
+	if err := exec.Command("ifconfig", "wg", "create", "name", name).Run(); err != nil {
+		return nil, err
 	}
 
-	return exec.Command("ifconfig", i.Name, "alias", ip.String(), "up").Run()
+	return FindDevice(name)
 }
 
-func (d *BSDKernelDevice) AddRoute(dst net.IPNet) error {
-	i, err := net.InterfaceByIndex(d.index)
+func FindDevice(name string) (KernelDevice, error) {
+	i, err := net.InterfaceByName(name)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return exec.Command("route", "add", "-net", dst.String(), "-interface", i.Name).Run()
+	return &BSDKernelDevice{
+		created: false,
+		index:   i.Index,
+		logger:  zap.L().Named("device").With(zap.String("dev", name)),
+	}, nil
+}
+
+func (d *BSDKernelDevice) Name() string {
+	i, err := net.InterfaceByIndex(d.index)
+	if err != nil {
+		panic(err)
+	}
+
+	return i.Name
+}
+
+func (d *BSDKernelDevice) Close() error {
+	return nil
+}
+
+func (d *BSDKernelDevice) AddAddress(ip *net.IPNet) error {
+
+	return exec.Command("ifconfig", d.Name(), ip.IP.String(), "netmask", ip.Mask.String(), "alias").Run()
+}
+
+func (d *BSDKernelDevice) DeleteAddress(ip *net.IPNet) error {
+	return exec.Command("ifconfig", d.Name(), ip.IP.String(), "netmask", ip.Mask.String(), "delete").Run()
+}
+
+func (d *BSDKernelDevice) AddRoute(dst *net.IPNet) error {
+	return exec.Command("route", "add", "-net", dst.String(), "-interface", d.Name()).Run()
+}
+
+func (d *BSDKernelDevice) DeleteRoute(dst *net.IPNet) error {
+	return exec.Command("route", "delete", "-net", dst.String(), "-interface", d.Name()).Run()
+
 }
 
 func (d *BSDKernelDevice) Index() int {
