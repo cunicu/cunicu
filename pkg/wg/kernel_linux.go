086d9bbe3458033553979400cb0a884264676b9a
Steffen Vogel <post@steffenvogel.de>
2022-08-04 03:21:11 +0200

    fix spelling of WireGuard
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/wg/kernel_linux.go
+++ pkg/wg/kernel_linux.go
@@ -17,11 +17,11 @@ func KernelModuleExists() bool {
 	// We willingly try to create a device with an invalid
 	// MTU here as the validation of the MTU will be performed after
 	// the validation of the link kind and hence allows us to check
-	// for the existence of the wireguard module without actually
+	// for the existence of the WireGuard module without actually
 	// creating a link.
 	//
 	// As a side-effect, this will also let the kernel lazy-load
-	// the wireguard module.
+	// the WireGuard module.
 	l.LinkAttrs.MTU = math.MaxInt
 
 	err := netlink.LinkAdd(l)
