02f7e0f4a11503224eb3b9f519bbd1fcf134d5e3
Steffen Vogel <post@steffenvogel.de>
2022-08-16 16:26:32 +0200

    wg: do not fail on missing dirs/sockets for socket cleanup
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/wg/cleanup_unix.go
+++ pkg/wg/cleanup_unix.go
@@ -3,8 +3,10 @@
 package wg
 
 import (
+	"errors"
 	"io/fs"
 	"net"
+	"os"
 	"path"
 	"path/filepath"
 
@@ -15,6 +17,10 @@ import (
 func CleanupUserSockets() error {
 	logger := zap.L().Named("wg")
 
+	if _, err := os.Stat(SocketPath); err != nil && errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+
 	return filepath.Walk(SocketPath, func(p string, i fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -23,7 +29,7 @@ func CleanupUserSockets() error {
 		if !i.IsDir() && path.Ext(p) == ".sock" {
 			if c, err := net.Dial("unix", p); err == nil {
 				c.Close()
-			} else {
+			} else if !errors.Is(err, unix.ENOENT) {
 				logger.Warn("Delete stale WireGuard user socket", zap.String("path", p))
 
 				return unix.Unlink(p)
