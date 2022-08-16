e717dff11a419d02fd5310412c8d7ca2d556b469
Steffen Vogel <post@steffenvogel.de>
2022-08-16 16:27:30 +0200

    config: cleanup and removal of unused settings
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/config/defaults.go
+++ pkg/config/defaults.go
@@ -1,15 +1,14 @@
 package config
 
+import "riasc.eu/wice/pkg/wg"
+
 const (
-	DefaultSocketPath    = "/var/run/wice.sock"
-	DefaultSocketAddress = ":8810"
-	DefaultURL           = "stun:stun.l.google.com:19302"
+	DefaultSocketPath = "/var/run/wice.sock"
+	DefaultURL        = "stun:stun.l.google.com:19302"
 
 	// Ephemeral Port Range (RFC6056 Sect. 2.1)
 	EphemeralPortMin = (1 << 15) + (1 << 14)
 	EphemeralPortMax = (1 << 16) - 1
-
-	WireGuardDefaultPort = 51820
 )
 
 var (
@@ -17,28 +16,27 @@ var (
 )
 
 func (c *Config) SetDefaults() {
-	c.SetDefault("backends", DefaultBackends)
-	c.SetDefault("watch_interval", "1s")
-	c.SetDefault("socket.path", DefaultSocketPath)
-	c.SetDefault("socket.address", DefaultSocketAddress)
-	c.SetDefault("socket.wait", false)
 	c.SetDefault("auto_config.enabled", true)
+	c.SetDefault("backends", DefaultBackends)
 	c.SetDefault("config_sync.enabled", true)
 	c.SetDefault("config_sync.path", wg.ConfigPath)
 	c.SetDefault("config_sync.watch", false)
+	c.SetDefault("endpoint_disc.enabled", true)
+	c.SetDefault("endpoint_disc.ice.check_interval", "200ms")
+	c.SetDefault("endpoint_disc.ice.disconnected_timeout", "5s")
+	c.SetDefault("endpoint_disc.ice.failed_timeout", "5s")
+	c.SetDefault("endpoint_disc.ice.keepalive_interval", "2s")
+	c.SetDefault("endpoint_disc.ice.max_binding_requests", 7)
+	c.SetDefault("endpoint_disc.ice.port.max", EphemeralPortMax)
+	c.SetDefault("endpoint_disc.ice.port.min", EphemeralPortMin)
+	c.SetDefault("endpoint_disc.ice.restart_timeout", "5s")
+	c.SetDefault("endpoint_disc.ice.urls", []string{DefaultURL})
+	c.SetDefault("host_sync.enabled", true)
 	c.SetDefault("route_sync.enabled", true)
 	c.SetDefault("route_sync.table", "main")
-	c.SetDefault("host_sync.enabled", true)
-	c.SetDefault("endpoint_disc.enabled", true)
-	c.SetDefault("wireguard.port.min", WireGuardDefaultPort)
+	c.SetDefault("socket.path", DefaultSocketPath)
+	c.SetDefault("socket.wait", false)
+	c.SetDefault("watch_interval", "1s")
 	c.SetDefault("wireguard.port.max", EphemeralPortMax)
-	c.SetDefault("endpoint_disc.ice.check_interval", "200ms")
-	c.SetDefault("endpoint_disc.ice.keepalive_interval", "2s")
-	c.SetDefault("endpoint_disc.ice.disconnected_timeout", "5s")
-	c.SetDefault("endpoint_disc.ice.restart_timeout", "5s")
-	c.SetDefault("endpoint_disc.ice.failed_timeout", "5s")
-	c.SetDefault("endpoint_disc.ice.max_binding_requests", 7)
-	c.SetDefault("endpoint_disc.ice.urls", []string{DefaultURL})
-	c.SetDefault("endpoint_disc.ice.port.min", EphemeralPortMin)
-	c.SetDefault("endpoint_disc.ice.port.max", EphemeralPortMax)
+	c.SetDefault("wireguard.port.min", wg.DefaultPort)
 }
