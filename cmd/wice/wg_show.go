10db581c810cb706ed1241f08ae121c1fda492fd
Steffen Vogel <post@steffenvogel.de>
2022-08-26 14:00:53 +0200

    cmd: rework wg subcommands and add full compatability for 'wg show'
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ cmd/wice/wg_show.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"riasc.eu/wice/pkg/pb"
+	"riasc.eu/wice/pkg/wg"
+)
+
+var (
+	wgShowCmd = &cobra.Command{
+		// Use:   "show { <interface> | all | interfaces } [public-key | private-key | listen-port | fwmark | peers | preshared-keys | endpoints | allowed-ips | latest-handshakes | transfer | persistent-keepalive | dump]",
+		Use:   "show [flags] { <interface> | all | interfaces } [public-key | private-key | listen-port | fwmark | peers | preshared-keys | endpoints | allowed-ips | latest-handshakes | transfer | persistent-keepalive | dump]",
+		Short: "Shows the current configuration and device information",
+		RunE:  wgShow,
+		Args:  cobra.MaximumNArgs(2),
+	}
+)
+
+func init() {
+	addClientCommand(wgCmd, wgShowCmd)
+}
+
+func wgShow(cmd *cobra.Command, args []string) error {
+	var intf, mode, field string
+
+	if len(args) > 0 {
+		switch args[0] {
+		case "all":
+			mode = args[0]
+		case "interfaces":
+			mode = args[0]
+		default:
+			mode = "interface"
+			intf = args[0]
+		}
+
+		if len(args) > 1 {
+			switch args[1] {
+			case "public-key":
+			case "private-key":
+			case "listen-port":
+			case "fwmark":
+			case "peers":
+			case "preshared-keys":
+			case "endpoints":
+			case "allowed-ips":
+			case "latest-handshakes":
+			case "transfer":
+			case "persistent-keepalive":
+			case "dump":
+			default:
+				cmd.Usage()
+				os.Exit(1)
+			}
+			field = args[1]
+		} else {
+			field = "all"
+		}
+	}
+
+	sts, err := rpcClient.GetStatus(context.Background(), &pb.StatusParams{
+		Intf: intf,
+	})
+	if err != nil {
+		return fmt.Errorf("failed RPC request: %w", err)
+	}
+
+	intfNames := []string{}
+
+	for i, intf := range sts.Interfaces {
+		if mode == "interfaces" {
+			intfNames = append(intfNames, intf.Name)
+		} else {
+			var prefix string
+
+			mdev := intf.Device()
+			wdev := wg.Device(*mdev)
+
+			if mode == "all" {
+				prefix = wdev.Name + "\t"
+			}
+
+			if field == "all" {
+				if i > 0 {
+					if _, err := fmt.Println(); err != nil {
+						return err
+					}
+				}
+
+				if err := wdev.DumpEnv(os.Stdout); err != nil {
+					return err
+				}
+			}
+
+			var value any
+			switch field {
+			case "public-key":
+				value = wdev.PublicKey
+			case "private-key":
+				value = wdev.PrivateKey
+			case "listen-port":
+				value = wdev.ListenPort
+			case "fwmark":
+				value = "off"
+				if wdev.FirewallMark != 0 {
+					value = fmt.Sprint(wdev.FirewallMark)
+				}
+			}
+
+			if value != nil {
+				fmt.Printf("%s%v\n", prefix, value)
+			} else {
+				if field == "dump" {
+					fwmark := "off"
+					if wdev.FirewallMark != 0 {
+						fwmark = fmt.Sprint(wdev.FirewallMark)
+					}
+
+					fmt.Printf("%s%s\t%s\t%d\t%s\n",
+						prefix,
+						wdev.PrivateKey,
+						wdev.PublicKey,
+						wdev.ListenPort,
+						fwmark,
+					)
+				}
+
+				for _, peer := range wdev.Peers {
+					switch field {
+					case "peers":
+						fmt.Printf("%s%s\n", prefix, peer.PublicKey)
+						continue
+					case "preshared-keys":
+						value = peer.PresharedKey
+					case "endpoints":
+						value = peer.Endpoint
+					case "allowed-ips":
+						aips := []string{}
+						for _, aip := range peer.AllowedIPs {
+							aips = append(aips, aip.String())
+						}
+						value = strings.Join(aips, " ")
+					case "latest-handshakes":
+						value = peer.LastHandshakeTime.Unix()
+						if peer.LastHandshakeTime.IsZero() {
+							value = 0
+						}
+					case "transfer":
+						value = fmt.Sprintf("%d\t%d", peer.ReceiveBytes, peer.TransmitBytes)
+					case "persistent-keepalive":
+						value = peer.PersistentKeepaliveInterval.Seconds()
+						if peer.PersistentKeepaliveInterval == 0 {
+							value = "off"
+						}
+					case "dump":
+						as := []string{}
+						for _, aip := range peer.AllowedIPs {
+							as = append(as, aip.String())
+						}
+						aIPs := strings.Join(as, ",")
+
+						zero := wgtypes.Key{}
+						psk := "(none)"
+						if peer.PresharedKey != zero {
+							psk = peer.PresharedKey.String()
+						}
+
+						ep := ""
+						if peer.Endpoint != nil {
+							ep = peer.Endpoint.String()
+						}
+
+						pka := "off"
+						if peer.PersistentKeepaliveInterval.Seconds() > 0 {
+							pka = fmt.Sprintf("%d", int(peer.PersistentKeepaliveInterval.Seconds()))
+						}
+
+						lhs := int64(0)
+						if !peer.LastHandshakeTime.IsZero() {
+							lhs = peer.LastHandshakeTime.Unix()
+						}
+
+						value = fmt.Sprintf("%s\t%s\t%s\t%s\t%d\t%d\t%d\t%s",
+							peer.PublicKey,
+							psk, ep, aIPs, lhs,
+							peer.ReceiveBytes,
+							peer.TransmitBytes,
+							pka,
+						)
+					}
+
+					fmt.Printf("%s%s\t%v\n", prefix, peer.PublicKey, value)
+				}
+			}
+		}
+	}
+
+	if mode == "interfaces" {
+		fmt.Println(strings.Join(intfNames, " "))
+	}
+
+	return nil
+}
