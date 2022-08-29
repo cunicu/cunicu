f12f80808a2a0381c8fa7a85ecc7be15a42bc725
Steffen Vogel <post@steffenvogel.de>
2022-08-30 01:25:57 +0200

    test: increase unit test coverage
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/config/types_test.go
+++ pkg/config/types_test.go
@@ -6,52 +6,98 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/pflag"
 	"riasc.eu/wice/pkg/config"
 )
 
-var _ = Context("regex", func() {
-	const re1Str = "[a-z]"
-	var re1 = regexp.MustCompile(re1Str)
+var _ = Context("types", func() {
+	Context("regex", func() {
+		const re1Str = "[a-z]"
+		const re2Str = "[a-z"
+		var re1 = regexp.MustCompile(re1Str)
 
-	It("Unmarshal", func() {
-		var re config.Regexp
-		err := re.UnmarshalText([]byte(re1Str))
+		It("unmarshal", func() {
+			var re config.Regexp
+			err := re.UnmarshalText([]byte(re1Str))
 
-		Expect(err).To(Succeed())
+			Expect(err).To(Succeed())
 
-		Expect(re.MatchString("1")).NotTo(BeTrue())
-		Expect(re.MatchString("a")).To(BeTrue())
+			Expect(re.MatchString("1")).NotTo(BeTrue())
+			Expect(re.MatchString("a")).To(BeTrue())
+		})
+
+		It("marshal", func() {
+			re := config.Regexp{*re1}
+
+			reStr, err := re.MarshalText()
+			Expect(err).To(Succeed())
+			Expect(string(reStr)).To(Equal(re1Str), "MarshalText: %s != %s", string(reStr), re1Str)
+		})
+
+		It("fails on invalid regex", func() {
+			var re config.Regexp
+			err := re.UnmarshalText([]byte(re2Str))
+			Expect(err).To(HaveOccurred())
+		})
 	})
 
-	It("Marshal", func() {
-		re := config.Regexp{*re1}
+	Context("backend url", func() {
+		urls := map[string]config.BackendURL{
+			"https://example.com": {
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "example.com",
+				},
+			},
+			"p2p": {
+				URL: url.URL{
+					Scheme: "p2p",
+				},
+			},
+		}
 
-		reStr, err := re.MarshalText()
-		Expect(err).To(Succeed())
-		Expect(string(reStr)).To(Equal(re1Str), "MarshalText: %s != %s", string(reStr), re1Str)
+		for urlStr, urlExp := range urls {
+			Context("works for valid urls", func() {
+				It("unmarshal", func() {
+					var u config.BackendURL
+					err := u.UnmarshalText([]byte(urlStr))
+
+					Expect(err).To(Succeed())
+					Expect(u).To(Equal(urlExp))
+				})
+
+				It("marshal", func() {
+					u, err := urlExp.MarshalText()
+					Expect(err).To(Succeed())
+					Expect(u).To(BeEquivalentTo(urlStr))
+				})
+			})
+		}
+
+		It("fails for invalid urls", func() {
+			var u config.BackendURL
+			err := u.UnmarshalText([]byte("-"))
+			Expect(err).To(HaveOccurred())
+		})
 	})
-})
 
-var _ = Context("backend url", func() {
-	const urlStr = "https://example.com"
-	var urlExp = config.BackendURL{
-		URL: url.URL{
-			Scheme: "https",
-			Host:   "example.com",
-		},
-	}
+	Context("output format", func() {
+		for _, f := range config.OutputFormats {
+			It(f.String(), func() {
+				var g config.OutputFormat
 
-	It("Unmarshal", func() {
-		var u config.BackendURL
-		err := u.UnmarshalText([]byte(urlStr))
+				flags := pflag.NewFlagSet("test", pflag.ExitOnError)
+				flags.Var(&g, "format", "Output format")
 
-		Expect(err).To(Succeed())
-		Expect(u).To(Equal(urlExp))
+				err := flags.Parse([]string{"--format", f.String()})
+				Expect(err).To(Succeed())
+				Expect(g).To(BeEquivalentTo(f))
+
+				h, err := f.MarshalText()
+				Expect(err).To(Succeed())
+				Expect(h).To(BeEquivalentTo(f.String()))
+			})
+		}
 	})
 
-	It("Marshal", func() {
-		u, err := urlExp.MarshalText()
-		Expect(err).To(Succeed())
-		Expect(u).To(BeEquivalentTo(urlStr))
-	})
 })
