108a621908b18e671053dc3783834c5525216976
Steffen Vogel <post@steffenvogel.de>
2022-09-06 03:18:56 +0200

    watcher: fix tests
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/watcher/watcher_linux_test.go
+++ pkg/watcher/watcher_linux_test.go
@@ -5,7 +5,6 @@ package watcher_test
 import (
 	"fmt"
 	"math/rand"
-	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -24,13 +23,16 @@ var _ = Describe("watcher", func() {
 	var err error
 	var c *wgctrl.Client
 	var w *watcher.Watcher
-	var user bool
+	var devUser bool
 	var h *core.EventsHandler
 	var ns *g.Namespace
+	var devName string
 
 	JustBeforeEach(OncePerOrdered, func() {
-		name := fmt.Sprintf("wg-test-%d", rand.Intn(1000))
-		ns, err = g.NewNamespace(name)
+		By("Creating net namespace")
+
+		nsName := fmt.Sprintf("wg-test-ns-%d", rand.Intn(1000))
+		ns, err = g.NewNamespace(nsName)
 		Expect(err).To(Succeed())
 
 		func() {
@@ -43,14 +45,16 @@ var _ = Describe("watcher", func() {
 			Expect(err).To(Succeed())
 		}()
 
-		w, err = watcher.New(c, time.Hour, func(dev string) bool { return dev == "wg0" })
+		By("Creating watcher")
+
+		w, err = watcher.New(c, 0, func(dev string) bool { return dev == devName })
 		Expect(err).To(Succeed())
 
 		h = core.NewEventsHandler(16)
 
 		w.OnAll(h)
 
-		go w.Run()
+		go w.Watch()
 	})
 
 	JustAfterEach(OncePerOrdered, func() {
@@ -72,143 +76,159 @@ var _ = Describe("watcher", func() {
 		})
 	}
 
-	Test := func() {
-		Describe("can watch changes", Ordered, func() {
-			var i *core.Interface
-			var p *core.Peer
-			var d device.Device
+	TestSync := func() {
+		var i *core.Interface
+		var p *core.Peer
+		var d device.Device
 
-			It("adding interface", func() {
-				d, err = device.NewDevice("wg0", user)
-				Expect(err).To(Succeed())
+		It("adding interface", func() {
+			d, err = device.NewDevice(devName, devUser)
+			Expect(err).To(Succeed())
 
-				err = w.Sync()
-				Expect(err).To(Succeed())
+			err = w.Sync()
+			Expect(err).To(Succeed())
 
-				var ie core.InterfaceAddedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&ie))
+			var ie core.InterfaceAddedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&ie))
 
-				i = ie.Interface
+			i = ie.Interface
 
-				Expect(ie.Interface).NotTo(BeNil())
-				Expect(ie.Interface.Name()).To(Equal("wg0"))
+			Expect(ie.Interface).NotTo(BeNil())
+			Expect(ie.Interface.Name()).To(Equal(devName))
+		})
+
+		It("modifying the interface", func() {
+			oldListenPort := i.ListenPort
+			newListenPort := oldListenPort + 1
+
+			err = i.Configure(&wg.Config{
+				Config: wgtypes.Config{
+					ListenPort: &newListenPort,
+				},
 			})
+			Expect(err).To(Succeed())
 
-			It("modifying the interface", func() {
-				oldListenPort := i.ListenPort
-				newListenPort := oldListenPort + 1
+			err = w.Sync()
+			Expect(err).To(Succeed())
 
-				err = i.Configure(&wg.Config{
-					Config: wgtypes.Config{
-						ListenPort: &newListenPort,
-					},
-				})
-				Expect(err).To(Succeed())
+			var ie core.InterfaceModifiedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&ie))
 
-				err = w.Sync()
-				Expect(err).To(Succeed())
+			Expect(ie.Interface).NotTo(BeNil())
+			Expect(ie.Interface.Name()).To(Equal(devName))
+			Expect(ie.Modified & core.InterfaceModifiedListenPort).NotTo(BeZero())
+			Expect(ie.Interface.ListenPort).To(Equal(newListenPort))
+			Expect(ie.Old.ListenPort).To(Equal(oldListenPort))
+		})
 
-				var ie core.InterfaceModifiedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&ie))
+		It("adding a peer", func() {
+			sk, err := crypto.GenerateKey()
+			Expect(err).To(Succeed())
 
-				Expect(ie.Interface).NotTo(BeNil())
-				Expect(ie.Interface.Name()).To(Equal("wg0"))
-				Expect(ie.Modified & core.InterfaceModifiedListenPort).NotTo(BeZero())
-				Expect(ie.Interface.ListenPort).To(Equal(newListenPort))
-				Expect(ie.Old.ListenPort).To(Equal(oldListenPort))
+			err = i.AddPeer(&wgtypes.PeerConfig{
+				PublicKey: wgtypes.Key(sk.PublicKey()),
 			})
+			Expect(err).To(Succeed())
 
-			It("adding a peer", func() {
-				sk, err := crypto.GenerateKey()
-				Expect(err).To(Succeed())
+			err = w.Sync()
+			Expect(err).To(Succeed())
 
-				err = i.AddPeer(&wgtypes.PeerConfig{
-					PublicKey: wgtypes.Key(sk.PublicKey()),
-				})
-				Expect(err).To(Succeed())
+			var ie core.InterfaceModifiedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&ie))
 
-				err = w.Sync()
-				Expect(err).To(Succeed())
+			Expect(ie.Modified & core.InterfaceModifiedPeers).NotTo(BeZero())
 
-				var ie core.InterfaceModifiedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&ie))
+			var pe core.PeerAddedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&pe))
 
-				Expect(ie.Modified & core.InterfaceModifiedPeers).NotTo(BeZero())
+			// Remember peer for further test subjects
+			p = pe.Peer
 
-				var pe core.PeerAddedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&pe))
+			Expect(pe.Peer.PublicKey()).To(Equal(sk.PublicKey()))
+		})
 
-				// Remember peer for further test subjects
-				p = pe.Peer
+		It("modifying a peer", func() {
+			psk, err := crypto.GeneratePrivateKey()
+			Expect(err).To(Succeed())
 
-				Expect(pe.Peer.PublicKey()).To(Equal(sk.PublicKey()))
-			})
+			err = p.SetPresharedKey(&psk)
+			Expect(err).To(Succeed())
 
-			It("modifying a peer", func() {
-				psk, err := crypto.GeneratePrivateKey()
-				Expect(err).To(Succeed())
+			err = w.Sync()
+			Expect(err).To(Succeed())
 
-				err = p.SetPresharedKey(&psk)
-				Expect(err).To(Succeed())
+			var pe core.PeerModifiedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&pe))
 
-				err = w.Sync()
-				Expect(err).To(Succeed())
+			Expect(pe.Peer.PublicKey()).To(Equal(p.PublicKey()))
+			Expect(pe.Modified & core.PeerModifiedPresharedKey).NotTo(BeZero())
+			Expect(pe.Peer.PresharedKey()).To(Equal(psk))
+		})
 
-				var pe core.PeerModifiedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&pe))
+		It("removing a peer", func() {
+			err := i.RemovePeer(p.PublicKey())
+			Expect(err).To(Succeed())
 
-				Expect(pe.Peer.PublicKey()).To(Equal(p.PublicKey()))
-				Expect(pe.Modified & core.PeerModifiedPresharedKey).NotTo(BeZero())
-				Expect(pe.Peer.PresharedKey()).To(Equal(psk))
-			})
+			err = w.Sync()
+			Expect(err).To(Succeed())
 
-			It("removing a peer", func() {
-				err := i.RemovePeer(p.PublicKey())
-				Expect(err).To(Succeed())
+			var ie core.InterfaceModifiedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&ie))
 
-				err = w.Sync()
-				Expect(err).To(Succeed())
+			Expect(ie.Modified & core.InterfaceModifiedPeers).NotTo(BeZero())
 
-				var ie core.InterfaceModifiedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&ie))
+			var pe core.PeerRemovedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&pe))
 
-				Expect(ie.Modified & core.InterfaceModifiedPeers).NotTo(BeZero())
+			Expect(pe.Peer.PublicKey()).To(Equal(p.PublicKey()))
+		})
 
-				var pe core.PeerRemovedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&pe))
+		It("removing an interface", func() {
+			err = d.Close()
+			Expect(err).To(Succeed())
 
-				Expect(pe.Peer.PublicKey()).To(Equal(p.PublicKey()))
-			})
+			err = w.Sync()
+			Expect(err).To(Succeed())
 
-			It("removing an interface", func() {
-				err = d.Close()
-				Expect(err).To(Succeed())
+			var ie core.InterfaceRemovedEvent
+			Expect(h.Events).To(test.ReceiveEvent(&ie))
 
-				err = w.Sync()
-				Expect(err).To(Succeed())
-
-				var ie core.InterfaceRemovedEvent
-				Expect(h.Events).To(test.ReceiveEvent(&ie))
-
-				Expect(ie.Interface).NotTo(BeNil())
-				Expect(ie.Interface.Name()).To(Equal("wg0"))
-			})
+			Expect(ie.Interface).NotTo(BeNil())
+			Expect(ie.Interface.Name()).To(Equal(devName))
 		})
 	}
 
-	Describe("kernel", func() {
-		BeforeEach(OncePerOrdered, func() {
-			user = false
+	Context("sync", func() {
+		Context("kernel", Ordered, func() {
+			BeforeAll(func() {
+				devName = "wg-kernel"
+				devUser = false
+			})
+
+			TestSync()
 		})
 
-		Test()
+		Context("user", Ordered, func() {
+			BeforeAll(func() {
+				devName = fmt.Sprintf("wg-user-%d", rand.Intn(1000))
+				devUser = true
+			})
+
+			TestSync()
+		})
 	})
 
-	Describe("user", func() {
-		BeforeEach(OncePerOrdered, func() {
-			user = true
+	Context("watch", Pending, func() {
+		Context("periodic", func() {
+
 		})
 
-		Test()
+		Context("user", func() {
+
+		})
+
+		Context("kernel", func() {
+
+		})
 	})
 })
