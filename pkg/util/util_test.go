20f35b2b80ac2539146f10d7e171ec0cbdd39e37
Steffen Vogel <post@steffenvogel.de>
2022-09-07 14:59:09 +0200

    util: add ContainsNet()
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -18,7 +18,6 @@ func TestSuite(t *testing.T) {
 var _ = test.SetupLogging()
 
 var _ = Describe("utils", func() {
-
 	Context("endpoint comparisons", func() {
 		It("to be equal", func() {
 			a := net.UDPAddr{
@@ -76,4 +75,23 @@ var _ = Describe("utils", func() {
 			Expect(util.CmpNet(a, b)).NotTo(BeZero())
 		})
 	})
+
+	_, net1, _ := net.ParseCIDR("1.1.1.1/24")
+	_, net2, _ := net.ParseCIDR("1.1.0.2/16")
+	_, net3, _ := net.ParseCIDR("1.1.1.3/25")
+	_, net4, _ := net.ParseCIDR("1.2.0.4/16")
+
+	DescribeTable("network intersection",
+		func(outer, inner *net.IPNet, expect bool) {
+			result := util.ContainsNet(outer, inner)
+			Expect(result).To(Equal(expect), "Expected a=%s, b=%s, expected=%t, result=%t", outer, inner, result, expect)
+		},
+		Entry("net1 contains net2 = true", net1, net2, false),
+		Entry("net2 contains net1 = true", net2, net1, true),
+		Entry("net1 contains net3 = true", net1, net3, true),
+		Entry("net3 contains net1 = true", net3, net1, false),
+		Entry("net1 contains net4 = false", net1, net4, false),
+		Entry("net4 contains net1 = false", net4, net1, false),
+		Entry("net1 contains net1 = true", net1, net1, true),
+	)
 })
