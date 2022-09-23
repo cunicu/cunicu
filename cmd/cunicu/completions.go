e0b52f82e0f66ae20c5f70f0cd55831f28ec5d7e
Steffen Vogel <post@steffenvogel.de>
2022-09-23 13:51:26 +0200

    config: move completions to main package
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- cmd/cunicu/completions.go
+++ cmd/cunicu/completions.go
@@ -1,7 +1,5 @@
-package config
+package main
 
 import "github.com/spf13/cobra"
-
-type ValidArgsFunction func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective)
 
 var BooleanCompletions = cobra.FixedCompletions([]string{"true", "false"}, cobra.ShellCompDirectiveNoFileComp)
