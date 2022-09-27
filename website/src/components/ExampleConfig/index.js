997841c741281abb3dd53d033cea8118524d0cd4
Steffen Vogel <post@steffenvogel.de>
2022-09-27 12:17:45 +0200

    website: add ExampleConfig component
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ website/src/components/ExampleConfig/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import CodeBlock from '@theme/CodeBlock';
+import ExampleConfigSource from '!!raw-loader!../../../../etc/cunicu.yaml';
+
+export default function ExampleConfig(props) {
+    let codeProps = {...props};
+    if (!codeProps.language) {
+        codeProps.language = 'yaml';
+    }
+
+    codeProps.title = '/etc/cunicu.yaml';
+
+    let content = ExampleConfigSource;
+
+    if (codeProps.section) {
+        const contentLines = content.split('\n');
+        let commentLines = [];
+        let sectionLines = [];
+        let inSection = false;
+
+        for (let line of contentLines) {
+            let startsSection = false;
+            let endsSection = false;
+            let commentLine = line.startsWith('#');
+            let emptyLine = line.trim() === '';
+
+            let matches = line.match(/^([a-zA-z]+):/);
+            if (matches !== null) {
+                startsSection = matches[1] == codeProps.section;
+                endsSection = matches[1] != codeProps.section;
+            }
+
+            if (commentLine) {
+                inSection = false;
+                commentLines.push(line);
+            }
+            
+            if (startsSection) {
+                inSection = true;
+
+                sectionLines.push(...commentLines);
+                commentLines = [];
+            }
+
+            if (endsSection)
+                inSection = false;
+
+            if (emptyLine)
+                commentLines = [];
+
+            if (inSection)
+                sectionLines.push(line);
+        }
+
+        if (sectionLines[sectionLines.length - 1] == '')
+            sectionLines = sectionLines.slice(0, -1);
+
+        content = sectionLines.join('\n');
+
+        codeProps.title = `Section "${codeProps.section}" of ${codeProps.title}`;
+    }
+
+    return <CodeBlock {...codeProps}>{content}</CodeBlock>;
+}
