77784d3e369765caff0cfb1a1b22dc64df9301e6
Steffen Vogel <post@steffenvogel.de>
2022-09-28 16:29:04 +0200

    ice: move ice package back to top level package dir
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/rpc/client.go
+++ pkg/rpc/client.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/stv0g/cunicu/pkg/core"
 	"github.com/stv0g/cunicu/pkg/crypto"
-	icex "github.com/stv0g/cunicu/pkg/feat/epdisc/ice"
+	icex "github.com/stv0g/cunicu/pkg/ice"
 	"github.com/stv0g/cunicu/pkg/proto"
 	rpcproto "github.com/stv0g/cunicu/pkg/proto/rpc"
 	"github.com/stv0g/cunicu/pkg/util/buildinfo"
