9e7183ec195db2600206ff621004ebf06ec488cd
Steffen Vogel <post@steffenvogel.de>
2022-10-01 09:46:38 +0200

    rtsync: add support for non-link-local routes
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/daemon/feature/rtsync/handlers.go
+++ pkg/daemon/feature/rtsync/handlers.go
@@ -2,21 +2,28 @@ package rtsync
 
 import (
 	"errors"
+	"fmt"
 	"net"
 	"net/netip"
 	"syscall"
 
 	"github.com/stv0g/cunicu/pkg/core"
+	"github.com/stv0g/cunicu/pkg/util"
 	"go.uber.org/zap"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 func (rs *Interface) OnPeerAdded(p *core.Peer) {
 	pk := p.PublicKey()
-	gwV4, ok1 := netip.AddrFromSlice(pk.IPv4Address().IP)
-	gwV6, ok2 := netip.AddrFromSlice(pk.IPv6Address().IP)
-	if !ok1 || !ok2 {
-		panic("failed to get address from slice")
+
+	gwV4, ok := netip.AddrFromSlice(pk.IPv4Address().IP)
+	if !ok {
+		panic(fmt.Errorf("failed to get address from slice: %s", pk.IPv4Address().IP))
+	}
+
+	gwV6, ok := netip.AddrFromSlice(pk.IPv6Address().IP)
+	if !ok {
+		panic(fmt.Errorf("failed to get address from slice: %s", pk.IPv6Address().IP))
 	}
 
 	rs.gwMap[gwV4] = p
@@ -29,10 +36,15 @@ func (rs *Interface) OnPeerAdded(p *core.Peer) {
 
 func (rs *Interface) OnPeerRemoved(p *core.Peer) {
 	pk := p.PublicKey()
-	gwV4, ok1 := netip.AddrFromSlice(pk.IPv4Address().IP)
-	gwV6, ok2 := netip.AddrFromSlice(pk.IPv6Address().IP)
-	if !ok1 || !ok2 {
-		panic("failed to get address from slice")
+
+	gwV4, ok := netip.AddrFromSlice(pk.IPv4Address().IP)
+	if !ok {
+		panic(fmt.Errorf("failed to get address from slice: %s", pk.IPv4Address().IP))
+	}
+
+	gwV6, ok := netip.AddrFromSlice(pk.IPv6Address().IP)
+	if !ok {
+		panic(fmt.Errorf("failed to get address from slice: %s", pk.IPv6Address().IP))
 	}
 
 	delete(rs.gwMap, gwV4)
@@ -48,14 +60,29 @@ func (rs *Interface) OnPeerRemoved(p *core.Peer) {
 }
 
 func (rs *Interface) OnPeerModified(p *core.Peer, old *wgtypes.Peer, m core.PeerModifier, ipsAdded, ipsRemoved []net.IPNet) {
+	pk := p.PublicKey()
+
 	for _, dst := range ipsAdded {
-		if err := p.Interface.KernelDevice.AddRoute(dst, rs.Settings.RouteSync.Table); err != nil {
+		var gwn net.IPNet
+		if isV6 := dst.IP.To4() == nil; isV6 {
+			gwn = pk.IPv6Address()
+		} else {
+			gwn = pk.IPv4Address()
+		}
+
+		var gw net.IP
+		if !util.ContainsNet(&gwn, &dst) {
+			gw = gwn.IP
+		}
+
+		if err := p.Interface.KernelDevice.AddRoute(dst, gw, rs.Settings.RouteSync.Table); err != nil {
 			rs.logger.Error("Failed to add route", zap.Error(err))
 			continue
 		}
 
 		rs.logger.Info("Added new AllowedIP to kernel routing table",
-			zap.Any("dst", dst),
+			zap.String("dst", dst.String()),
+			zap.Any("gw", gw.String()),
 			zap.Any("intf", p.Interface),
 			zap.Any("peer", p))
 	}
@@ -67,7 +94,7 @@ func (rs *Interface) OnPeerModified(p *core.Peer, old *wgtypes.Peer, m core.Peer
 		}
 
 		rs.logger.Info("Remove vanished AllowedIP from kernel routing table",
-			zap.Any("dst", dst),
+			zap.String("dst", dst.String()),
 			zap.Any("intf", p.Interface),
 			zap.Any("peer", p))
 	}
