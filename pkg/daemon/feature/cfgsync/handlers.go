da5ec230310de763b86ab36ef8d6701e708dd630
Steffen Vogel <post@steffenvogel.de>
2022-10-04 11:21:29 +0200

    cfgsync: start rewrite
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/daemon/feature/cfgsync/handlers.go
+++
@@ -1,9 +0,0 @@
-package cfgsync
-
-import (
-	"github.com/stv0g/cunicu/pkg/core"
-	"github.com/stv0g/cunicu/pkg/wg"
-)
-
-func (cs *Interface) OnInterfaceModified(i *core.Interface, old *wg.Device, m core.InterfaceModifier) {
-}
