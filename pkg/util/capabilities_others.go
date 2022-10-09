b8a96a175584ea8d46caf91cddf8e67f0ee44a92
Steffen Vogel <post@steffenvogel.de>
2022-10-09 09:54:34 +0200

    util: support more platforms for HasAdminPrivileges()
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/util/capabilities_others.go
+++ pkg/util/capabilities_others.go
@@ -1,7 +1,7 @@
-//go:build !linux
+//go:build !(linux || darwin || freebsd || openbsd || netbsd || plan9 || dragonfly || solaris)
 
 package util
 
 func HasAdminPrivileges() bool {
-	return false // TODO
+	return false
 }
