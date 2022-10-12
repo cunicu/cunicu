65a4303ae7f2ea0b741a43706814272788bb61c0
Steffen Vogel <post@steffenvogel.de>
2022-10-12 22:40:33 +0200

    device: add debug logging statements
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/device/device_freebsd.go
+++ pkg/device/device_freebsd.go
@@ -7,6 +7,10 @@ import (
 )
 
 func (d *BSDKernelDevice) AddRoute(dst net.IPNet, gw net.IP, table int) error {
+	d.logger.Debug("Add route",
+		zap.String("dst", dst.String()),
+		zap.String("gw", gw.String()))
+
 	if gw == nil {
 		return exec.Command("route", "add", "-net", dst.String(), "-interface", d.Name(), "-fib", fmt.Sprint(table)).Run()
 	} else {
@@ -15,5 +19,8 @@ func (d *BSDKernelDevice) AddRoute(dst net.IPNet, gw net.IP, table int) error {
 }
 
 func (d *BSDKernelDevice) DeleteRoute(dst net.IPNet, table int) error {
+	d.logger.Debug("Delete route",
+		zap.String("dst", dst.String()))
+
 	return exec.Command("route", "delete", "-net", dst.String(), "-interface", d.Name(), "-fib", fmt.Sprint(table)).Run()
 }
--- pkg/device/device_windows.go
+++ pkg/device/device_windows.go
@@ -11,18 +11,29 @@ type WindowsKernelDevice struct {
 }
 
 func (d *WindowsKernelDevice) AddAddress(ip net.IPNet) error {
+	d.logger.Debug("Add address", zap.String("addr", ip.String()))
+
 	return errors.ErrNotSupported
 }
 
 func (d *WindowsKernelDevice) AddRoute(dst net.IPNet, gw net.IP, table int) error {
+	i.logger.Debug("Add route",
+		zap.String("dst", dst.String()),
+		zap.String("gw", gw.String()))
+
 	return errors.ErrNotSupported
 }
 
 func (d *WindowsKernelDevice) DeleteAddress(ip net.IPNet) error {
+	i.logger.Debug("Delete address", zap.String("addr", ip.String()))
+
 	return errors.ErrNotSupported
 }
 
 func (d *WindowsKernelDevice) DeleteRoute(dst net.IPNet, table int) error {
+	i.logger.Debug("Delete route",
+		zap.String("dst", dst.String()))
+
 	return errors.ErrNotSupported
 }
 
@@ -36,19 +47,27 @@ func (d *WindowsKernelDevice) MTU() int {
 }
 
 func (d *WindowsKernelDevice) SetMTU(mtu int) error {
+	d.logger.Debug("Set link MTU", zap.Int("mtu", mtu))
+
 	// MTU is a route attribute which we need to adjust for all routes added for the interface
 	return errors.ErrNotSupported
 }
 
 func (d *WindowsKernelDevice) SetUp() error {
+	d.logger.Debug("Set link up")
+
 	return errors.ErrNotSupported
 }
 
 func (d *WindowsKernelDevice) SetDown() error {
+	i.logger.Debug("Set link down")
+
 	return errors.ErrNotSupported
 }
 
 func (d *WindowsKernelDevice) Close() error {
+	i.logger.Debug("Deleting kernel device")
+
 	return nil
 }
 
