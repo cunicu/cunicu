07c6053a415b6b90caaccd17a2cf8c3e9258aeec
Steffen Vogel <post@steffenvogel.de>
2023-05-27 09:28:03 +0200

    website: fix build in CI

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,181 +9,179 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const math = require("remark-math");
 const katex = require("rehype-katex");
 
-async function createConfig() {
-  const mdxMermaid = await import("mdx-mermaid");
+/** @type {import("@docusaurus/types").Config} */
+module.exports = {
+  title: "cunīcu",
+  tagline: "zeroconf • p2p • mesh • vpn",
+  url: "https://cunicu.li",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  trailingSlash: false,
 
-  /** @type {import("@docusaurus/types").Config} */
-  return {
-    title: "cunīcu",
-    tagline: "zeroconf • p2p • mesh • vpn",
-    url: "https://cunicu.li",
-    baseUrl: "/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.png",
-    trailingSlash: false,
-
-    stylesheets: [
-      {
-        href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-        type: "text/css",
-        integrity:
-          "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-        crossorigin: "anonymous",
-      },
-    ],
-
-    plugins: [
-      [
-        require.resolve("@cmfcmf/docusaurus-search-local"),
-        {
-          
-        }
-      ],
-      [
-        require.resolve("@gabrielcsapo/docusaurus-plugin-matomo"),
-        {
-          siteId: "5",
-          matomoUrl: "https://matomo.0l.de",
-          siteUrl: "https://cunicu.li",
-        }
-      ],
-      function (context, options) {
-        return {
-          name: "webpack-configuration-plugin",
-          configureWebpack(config, isServer, utils) {
-            return {
-              resolve: {
-                symlinks: false,
-              }
-            };
-          }
-        };
-      },
-    ],
-
-    // GitHub pages deployment config.
-    // If you aren"t using GitHub pages, you don't need these.
-    organizationName: "stv0g", // Usually your GitHub org/user name.
-    projectName: "cunicu", // Usually your repo name.
-
-    // Even if you don"t use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-      defaultLocale: "en",
-      locales: ["en"],
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
+  ],
 
-    presets: [
-      [
-        "classic",
-        /** @type {import("@docusaurus/preset-classic").Options} */
-        ({
-          docs: {
-            sidebarPath: require.resolve("./sidebars.js"),
-            editUrl:
-              "https://github.com/stv0g/cunicu/edit/master/",
-            remarkPlugins: [
-              mdxMermaid.default,
-              math
-            ],
-            rehypePlugins: [
-              katex
-            ],
-          },
-          blog: {
-            showReadingTime: true,
-            // Please change this to your repo.
-            // Remove this to remove the "edit this page" links.
-            editUrl:
-              "https://github.com/stv0g/cunicu/master/website",
-          },
-          theme: {
-            customCss: require.resolve("./src/css/custom.css"),
-          },
-        }),
-      ],
+  plugins: [
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+        
+      }
     ],
+    [
+      require.resolve("@gabrielcsapo/docusaurus-plugin-matomo"),
+      {
+        siteId: "5",
+        matomoUrl: "https://matomo.0l.de",
+        siteUrl: "https://cunicu.li",
+      }
+    ],
+    function (context, options) {
+      return {
+        name: "webpack-configuration-plugin",
+        configureWebpack(config, isServer, utils) {
+          return {
+            resolve: {
+              symlinks: false,
+            }
+          };
+        }
+      };
+    },
+  ],
 
-    themeConfig:
-      /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
+  // GitHub pages deployment config.
+  // If you aren"t using GitHub pages, you don't need these.
+  organizationName: "stv0g", // Usually your GitHub org/user name.
+  projectName: "cunicu", // Usually your repo name.
+
+  // Even if you don"t use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import("@docusaurus/preset-classic").Options} */
       ({
-        colorMode: {
-          disableSwitch: true
-        },
-        navbar: {
-          title: "cunīcu",
-          logo: {
-            alt: "cunīcu logo",
-            src: "img/cunicu_icon.svg",
-          },
-          items: [
-            {
-              type: "doc",
-              docId: "index",
-              position: "left",
-              label: "Docs",
-            },
-            {
-              to: "/blog",
-              label: "Blog",
-              position: "left"
-            },
-            {
-              href: "https://github.com/stv0g/cunicu",
-              label: "GitHub",
-              position: "right",
-            },
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/stv0g/cunicu/edit/master/",
+          remarkPlugins: [
+            math
+          ],
+          rehypePlugins: [
+            katex
           ],
         },
-        footer: {
-          style: "dark",
-          links: [
-            {
-              title: "Docs",
-              items: [
-                {
-                  label: "Tutorial",
-                  to: "/docs/",
-                },
-              ],
-            },
-            {
-              title: "Community",
-              items: [
-                {
-                  label: "Slack",
-                  href: "https://gophers.slack.com/archives/C036CTEGJFN",
-                },
-                {
-                  label: "Twitter",
-                  href: "https://twitter.com/cunicuVPN",
-                },
-              ],
-            },
-            {
-              title: "More",
-              items: [
-                {
-                  label: "Blog",
-                  to: "/blog",
-                },
-                {
-                  label: "GitHub",
-                  href: "https://github.com/stv0g/cunicu",
-                },
-              ],
-            },
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} Institute for Automation of Complex Power Systems, RWTH Aachen University.`,
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/stv0g/cunicu/master/website",
         },
-        prism: {
-          theme: lightCodeTheme,
-          darkTheme: darkCodeTheme,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
-  };
-}
+    ],
+  ],
 
-module.exports = createConfig;
+  themeConfig:
+    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
+    ({
+      colorMode: {
+        disableSwitch: true
+      },
+      navbar: {
+        title: "cunīcu",
+        logo: {
+          alt: "cunīcu logo",
+          src: "img/cunicu_icon.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            to: "/blog",
+            label: "Blog",
+            position: "left"
+          },
+          {
+            href: "https://github.com/stv0g/cunicu",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Slack",
+                href: "https://gophers.slack.com/archives/C036CTEGJFN",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/cunicuVPN",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/stv0g/cunicu",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Institute for Automation of Complex Power Systems, RWTH Aachen University.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+};
