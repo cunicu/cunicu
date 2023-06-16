f58b676b85e1a3ca0b730d2e9eb6e173eb685042
Philipp Jungkamp <p.jungkamp@gmx.net>
2023-06-16 12:41:33 +0200

    dist-nix: initial flake
    Signed-off-by: Philipp Jungkamp <p.jungkamp@gmx.net>

---
+++ scripts/update_nix_hash.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+file=./dist/nix/cunicu.nix
+
+printf '%s\n' 'Faking the hash'
+
+sed -i 's|vendorHash.*;$|vendorHash = lib.fakeHash;|' "$file"
+
+printf '%s\n' 'Evaluating the derivation'
+
+output="$(
+  nix build ./dist/nix#cunicu.go-modules \
+    --extra-experimental-features 'nix-command flakes' \
+    --refresh \
+    --no-link \
+    2>&1
+)"
+
+printf '%s\n' 'Extract correct hash'
+
+correct_hash="$(sed -n '$s|^\s*got:\s*||p' <<<"$output")"
+
+if [ -z "$correct_hash" ]; then
+	printf '%s\n' 'Error!' "$output"
+	exit 1
+fi
+
+printf '%s\n' "Set hash to $correct_hash"
+
+sed -i "s|vendorHash.*;$|vendorHash = \"$correct_hash\";|" "$file"
+
+nix build ./dist/nix#cunicu \
+  --extra-experimental-features 'nix-command flakes' \
+  --no-link
+
