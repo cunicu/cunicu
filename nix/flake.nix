213006798e9f6525c8223ca0517e4c75fdcea67c
Steffen Vogel <post@steffenvogel.de>
2023-06-16 13:42:39 +0200

    Move nix packaging to nix folder and add missing license / copyright notices
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2023 Philipp Jungkamp <p.jungkamp@gmx.net>
+# SPDX-License-Identifier: Apache-2.0
 {
   description = "cunīcu is a user-space daemon managing WireGuard® interfaces to establish a mesh of peer-to-peer VPN connections in harsh network environments.";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
