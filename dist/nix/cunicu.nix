e24ee5da495d2cfb50702185ac53e997d41987fe
Philipp Jungkamp <p.jungkamp@gmx.net>
2023-06-16 12:41:33 +0200

    dist-nix: initial flake
    Signed-off-by: Philipp Jungkamp <p.jungkamp@gmx.net>

---
+++ dist/nix/cunicu.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  src,
+  buildGoModule,
+}:
+buildGoModule rec {
+  pname = "cunicu";
+  version = "0.3.4";
+  vendorHash = "sha256-KXLKCwV3/9+PRQ1mYCZBvaV914WN68FgJXNWZizOmkA=";
+  inherit src;
+  CGO_ENABLED = 0;
+  # these packages contain networking dependent tests which fail in the sandbox
+  excludedPackages = ["pkg/config" "pkg/selfupdate" "pkg/tty"];
+  postBuild = ''
+    cunicu=$GOPATH/bin/cunicu
+    $cunicu docs --with-frontmatter
+  '';
+  postInstall = ''
+    install -d $out/usr/share/man/man1
+    install ./docs/usage/man/*.1 $out/usr/share/man/man1
+    install -D <($cunicu completion bash) $out/share/bash-completion/completions/cunicu
+    install -D <($cunicu completion fish) $out/share/fish/vendor_completions.d/cunicu.fish
+    install -D <($cunicu completion zsh) $out/share/zsh/vendor-completions/_cunicu
+  '';
+  meta = with lib; {
+    description = "A zeroconf peer-to-peer mesh VPN using Wireguard® and Interactive Connectivity Establishment (ICE)";
+    homepage = "https://cunicu.li";
+    license = licenses.asl20;
+  };
+}
---
+++ dist/nix/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "cunīcu is a user-space daemon managing WireGuard® interfaces to establish a mesh of peer-to-peer VPN connections in harsh network environments.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
+    forSystems = lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    pkgsFor = system: nixpkgs.legacyPackages.${system};
+    packagesWith = pkgs: {
+      cunicu = pkgs.callPackage ./cunicu.nix {
+        src = ../..;
+      };
+    };
+  in {
+    packages = forSystems (system: packagesWith (pkgsFor system) // {default = self.packages.${system}.cunicu;});
+    formatter = forSystems (system: (pkgsFor system).alejandra);
+    overlays.default = final: prev: packagesWith final;
+  };
+}
