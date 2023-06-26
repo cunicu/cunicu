c5d559cb5f714083df1010202a9da7ad5ccbddf4
Steffen Vogel <post@steffenvogel.de>
2023-06-26 21:08:40 +0200

    epdisc: update NAT rules on change of listen-port
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/daemon/feature/epdisc/nat.go
+++ pkg/daemon/feature/epdisc/nat.go
@@ -10,9 +10,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func (i *Interface) setupNAT() error {
-	var err error
-
+func (i *Interface) setupNAT() (err error) {
 	ident := fmt.Sprintf("cunicu-if%d", i.Device.Index())
 	if i.nat, err = NewNAT(ident); err != nil {
 		if !errors.Is(err, errNotSupported) {
@@ -20,20 +18,43 @@ func (i *Interface) setupNAT() error {
 		}
 	}
 
+	return i.updateNATRules()
+}
+
+func (i *Interface) updateNATRules() (err error) {
+	if i.ListenPort == 0 {
+		i.logger.Warn("Skipping setup of NAT rules as interface has no listen port yet")
+		return nil
+	}
+
 	ports := []int{}
 
 	// Redirect non-STUN traffic directed at UDP muxes to WireGuard interface via in-kernel port redirect / NAT
 	if i.mux != nil {
-		if i.natRule, err = i.nat.RedirectNonSTUN(i.muxPort, i.ListenPort); err != nil {
-			return fmt.Errorf("failed to setup port redirect for server reflexive UDP mux: %w", err)
+		if i.natRule != nil {
+			if err := i.natRule.Delete(); err != nil {
+				return fmt.Errorf("failed to remove port redirect for hots UDP mux: %w", err)
+			}
 		}
+
+		if i.natRule, err = i.nat.RedirectNonSTUN(i.muxPort, i.ListenPort); err != nil {
+			return fmt.Errorf("failed to setup port redirect for host UDP mux: %w", err)
+		}
+
 		ports = append(ports, i.muxPort)
 	}
 
 	if i.muxSrflx != nil {
+		if i.natRuleSrflx != nil {
+			if err := i.natRuleSrflx.Delete(); err != nil {
+				return fmt.Errorf("failed to remove port redirect for server reflexive UDP mux: %w", err)
+			}
+		}
+
 		if i.natRuleSrflx, err = i.nat.RedirectNonSTUN(i.muxSrflxPort, i.ListenPort); err != nil {
 			return fmt.Errorf("failed to setup port redirect for server reflexive UDP mux: %w", err)
 		}
+
 		ports = append(ports, i.muxSrflxPort)
 	}
 
