95602c514f4f46e5dab5f2916d860e3feefd5256
Steffen Vogel <post@steffenvogel.de>
2023-07-04 07:16:51 +0200

    website: Add mesh animation to banner
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -7,14 +7,20 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import MeshAnimation from '../components/MeshAnimation';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <BrowserOnly>
+        {() => {
+          return <MeshAnimation />
+        }}
+      </BrowserOnly>
+      <div className="container" style={{zIndex: 1}}>
         <img className="project-logo" style={{width: '40%'}} src="img/cunicu_logo_dark.svg" alt={siteConfig.title + ' - ' + siteConfig.tagline} />
         <div className={styles.buttons}>
           <Link
