bccb52cc4f23b9a4c6a8214f1f50a17fd43fe52f
Steffen Vogel <post@steffenvogel.de>
2024-06-14 11:28:32 +0200

    docs: Show comments only on post page not blog page overview
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- website/src/theme/BlogPostItem.js
+++ website/src/theme/BlogPostItem.js
@@ -2,14 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React from 'react';
+import {useBlogPost} from '@docusaurus/theme-common/internal';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import DiscourseComments from '@site/src/DiscourseComments';
 
 export default function BlogPostItemWrapper(props) {
+  const {isBlogPostPage} = useBlogPost();
   return (
     <>
       <BlogPostItem {...props} />
-      <DiscourseComments />
+      {isBlogPostPage && <DiscourseComments />}
     </>
   );
 }
