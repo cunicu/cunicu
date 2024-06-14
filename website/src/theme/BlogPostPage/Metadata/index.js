a0c0aa098e7071e9b04662defdfd60e1f2fb2994
Steffen Vogel <post@steffenvogel.de>
2024-06-14 11:28:32 +0200

    docs: Show comments only on post page not blog page overview
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- website/src/theme/BlogPostPage/Metadata/index.js
+++ website/src/theme/BlogPostPage/Metadata/index.js
@@ -4,13 +4,9 @@
 import React from 'react';
 import {PageMetadata} from '@docusaurus/theme-common';
 import Metadata from '@theme-original/BlogPostPage/Metadata';
-import type MetadataType from '@theme/BlogPostPage/Metadata';
-import type {WrapperProps} from '@docusaurus/types';
 import {useBlogPost} from '@docusaurus/theme-common/internal';
 
-type Props = WrapperProps<typeof MetadataType>;
-
-export default function MetadataWrapper(props: Props): JSX.Element {
+export default function MetadataWrapper(props) {
   const post = useBlogPost();
   const username = post.metadata.authors[0].discourse || "";
 
