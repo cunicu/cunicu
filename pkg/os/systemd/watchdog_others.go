c3a6b6e2bb4c51209f8fd8bb832809a2c91b26ac
Steffen Vogel <post@steffenvogel.de>
2024-12-24 12:10:38 +0100

    feat(systemd): Add support for watchdog timer
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ pkg/os/systemd/watchdog_others.go
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2023-2024 Steffen Vogel <post@steffenvogel.de>
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build !linux
+
+package systemd
+
+import "time"
+
+func WatchdogEnabled(_ bool) (interval time.Duration, err error) {
+	return 0, nil
+}
