3007e2a69beaa49203a94e711cf20b63e172c706
Steffen Vogel <post@steffenvogel.de>
2025-01-02 00:36:55 +0100

    feat(daemon): Allow passing systemd socket to RPC server
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/net/listen.go
+++ pkg/net/listen.go
@@ -8,13 +8,17 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"os"
 	"strings"
+
+	"cunicu.li/cunicu/pkg/os/systemd"
 )
 
 var (
-	errInvalidPortRange = errors.New("minimal port must be larger than maximal port number")
-	errInvalidNetwork   = errors.New("unsupported network")
-	errNoPortFound      = errors.New("failed to find port")
+	errInvalidPortRange   = errors.New("minimal port must be larger than maximal port number")
+	errInvalidNetwork     = errors.New("unsupported network")
+	errNoPortFound        = errors.New("failed to find port")
+	errNoSystemdListeners = errors.New("no file descriptors passed from systemd")
 )
 
 func FindRandomPortToListen(network string, mini, maxi int) (int, error) {
@@ -60,4 +64,58 @@ func canListenOnPort(network string, port int) bool {
 	}
 
 	return false
+}
+
+func Listen(socket string) (l net.Listener, err error) {
+	var network, address string
+
+	if p := strings.SplitN(socket, ":", 2); len(p) >= 2 {
+		network = p[0]
+		address = p[1]
+	} else if p[0] == "systemd" { //nolint:goconst
+		network = p[0]
+	} else {
+		network = "unix"
+		address = p[0]
+	}
+
+	switch {
+	case network == "systemd" && address == "":
+		sdListeners, err := systemd.Listeners()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get listeners from systemd: %w", err)
+		}
+
+		if len(sdListeners) == 0 {
+			return nil, errNoSystemdListeners
+		}
+
+		l = sdListeners[0]
+
+	case network == "systemd" && address != "":
+		sdListeners, err := systemd.ListenersWithNames()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get listeners from systemd: %w", err)
+		}
+
+		if ls, ok := sdListeners[address]; !ok || len(ls) == 0 {
+			return nil, fmt.Errorf("%w: with name %s", errNoSystemdListeners, address)
+		} else {
+			l = ls[0]
+		}
+
+	case network == "unix":
+		if err := os.RemoveAll(address); err != nil {
+			return nil, fmt.Errorf("failed to remove old socket: %w", err)
+		}
+
+		fallthrough
+
+	default:
+		if l, err = net.Listen(network, address); err != nil {
+			return nil, fmt.Errorf("failed to listen at %s: %w", socket, err)
+		}
+	}
+
+	return l, nil
 }
