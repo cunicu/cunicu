31d3348ee9bde7f8b905a238380a074a17c5fa42
Adam Rizkalla <ajarizzo@gmail.com>
2025-03-10 20:13:08 +0000

    feat(mcast-backend): add config parsing and update test
    Signed-off-by: Adam Rizkalla <ajarizzo@gmail.com>

--- pkg/signaling/mcast/config.go
+++ pkg/signaling/mcast/config.go
@@ -4,23 +4,31 @@
 package mcast
 
 import (
+	"fmt"
+	"net"
+
 	"cunicu.li/cunicu/pkg/signaling"
 )
+
+type BackendOptions struct {
+	Interface *net.Interface
+	Loopback  bool
+}
 
 type BackendConfig struct {
 	signaling.BackendConfig
 
-	Target   string
-	Loopback bool
+	Target  string
+	Options BackendOptions
 }
 
 func (c *BackendConfig) Parse(cfg *signaling.BackendConfig) (err error) {
 	c.BackendConfig = *cfg
 
-	//c.Target, c.Loopback, err = ParseURL(c.BackendConfig.URI.String())
-	//if err != nil {
-	//	return fmt.Errorf("failed to parse multicast URL: %w", err)
-	//}
+	c.Target, c.Options, err = ParseURL(c.BackendConfig.URI.String())
+	if err != nil {
+		return fmt.Errorf("failed to parse multicast URL: %w", err)
+	}
 
 	return nil
 }
